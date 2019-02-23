68a3e262934d8d8a029ee837094a2da7fc66f828
nnastos <36231428+nnastos@users.noreply.github.com>
2019-02-23 13:36:43 +0200

    End of Section 6

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators';
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  // this are properties
 baseUrl = 'http://localhost:5000/api/auth/';
+jwtHelper = new JwtHelperService();
+decodedToken: any;
+
+
 constructor(private http: HttpClient) { }
 
 login(model: any) {
@@ -16,6 +22,10 @@ login(model: any) {
         const user = response;
         if (user) {
           localStorage.setItem('token', user.token);
+          // we can see the result of the decoden token if we click console in google
+          // at this point we can see the name only when we are logged in and not when we refresh the page
+          this.decodedToken = this.jwtHelper.decodeToken(user.token);
+          console.log(this.decodedToken);
         }
       })
     );
@@ -24,5 +34,14 @@ login(model: any) {
 register(model: any) {
   return this.http.post(this.baseUrl + 'register', model);
 }
+
+loggedIn(){
+  const token = localStorage.getItem('token');
+  // if token is expired then is going to return true else is going to return false
+  return !this.jwtHelper.isTokenExpired(token);
+
+}
+
+
 
 }
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,29 +10,31 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login() {
     this.authService.login(this.model).subscribe(next => {
-      console.log('Logged in successfully');
+      this.alertify.success('Logged in successfully');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
   loggedIn() {
+    // we have injected in constructor the authService so we are going to use it for the logged in
 
-    const token = localStorage.getItem('token');
-    return !!token;
+    // so we are going to call the method loggedIn from authService
+
+    return this.authService.loggedIn();
   }
 
   logOut() {
 
     localStorage.removeItem('token');
-    console.log('logged out');
+    this.alertify.message('logged out');
   }
 
 }
